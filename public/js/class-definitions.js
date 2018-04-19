747e1d9954622ce878fefa5caa5b1d56372aee8f
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-18 19:01:26 -1000

    Meal class

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1098,6 +1098,11 @@ class Box{
  * @param {Array} foods All the foods in the meal
  */
 
+ class Meal{
+   constructor(foods){
+     this.foods = foods;
+   }
+ }
 
 /**
  * Create a new instance of the Classes you defined above, below!
