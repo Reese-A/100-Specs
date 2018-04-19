7af34d1f86ef92c394ec4f62ac50f26cdd40e51a
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-18 19:08:06 -1000

    Nemo

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1111,8 +1111,8 @@ class Box{
 
 
 // Create 2 different species of animals
-var george;
-var nemo;
+var george =  new Animal('Monkey', 'male');
+var nemo = new Animal('Fish', 'male');
 
 // Create 2 different vehicles
 var civic;
